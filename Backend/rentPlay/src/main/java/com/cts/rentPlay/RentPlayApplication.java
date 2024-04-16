01cbc6d54780e0c37518c70111a2a98092939182
adityakammari19 <adityakammari@gmail.com>
2024-04-16 13:11:45 +0530

    testing office commit

--- Backend/rentPlay/src/main/java/com/cts/rentPlay/RentPlayApplication.java
+++ Backend/rentPlay/src/main/java/com/cts/rentPlay/RentPlayApplication.java
@@ -5,12 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class RentPlayApplication {
